ed894a756c98a319019947b14d600df0101d78e3
gskielian <gregory.kielian@gmail.com>
2015-08-28 22:46:08 -0700

    added opencv dependencies, and voice interactions

--- app/src/main/java/net/kielian/www/jargg/MainActivity.java
+++ app/src/main/java/net/kielian/www/jargg/MainActivity.java
@@ -1,5 +1,22 @@
 package net.kielian.www.jargg;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.imgproc.Imgproc;
+
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
